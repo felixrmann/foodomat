4e8f5062684128559da5b0c1a5aaf8985a684d36
felixmann <felixmann00@gmail.com>
2024-10-03 22:47:56 +0200

    start of nav rework

--- src/foodomat-ui/src/app/components/foodomat-nav/foodomat-nav.component.ts
+++ src/foodomat-ui/src/app/components/foodomat-nav/foodomat-nav.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class FoodomatNavComponent {
 
+  private _active: boolean = false;
+
+  public get active(): boolean {
+    return this._active;
+  }
+
+  public set active(newValue: boolean) {
+    console.log(newValue);
+    this._active = newValue;
+  }
+
 }
